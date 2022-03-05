9dc6ad30599b8b72e846fcdf56cfb89e0c5ccd18
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2022-03-05 16:35:08 -0300

    Create imagens.py

---
+++ imagens.py
@@ -0,0 +1,6 @@
+#Imagens
+#Imagens locais
+#Você pode fazer referência a imagens locais e elas serão copiadas e renderizadas em seu blog automaticamente. Você pode incluí-los com a seguinte sintaxe de remarcação:
+
+
+![](my_icons/fastai_logo.png)
