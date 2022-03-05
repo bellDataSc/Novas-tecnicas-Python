7b5135cd69a18adfba0793c38fd1c56ebd405102
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2022-03-05 16:33:41 -0300

    Create Data-Tables.py

---
+++ Data-Tables.py
@@ -0,0 +1,5 @@
+#Você pode exibir tabelas da maneira usual em seu blog:
+# mesa de exibição com pandas
+
+df[['Title', 'Worldwide_Gross', 
+    'Production_Budget', 'Distributor', 'MPAA_Rating', 'IMDB_Rating', 'Rotten_Tomatoes_Rating']].head()
